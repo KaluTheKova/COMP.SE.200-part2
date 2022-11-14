e91a2d90cdd5ef1043a0388186190a4368d4d304
KaluTheKova <kalle.puutio@gmail.com>
2022-11-14 14:01:25 +0200

    kalle-tests.js 0.1

--- test/kalle-tests.js
+++ test/kalle-tests.js
@@ -7,21 +7,27 @@ ceil.js
 */
 // https://www.chaijs.com/guide/styles/#expect
 
-const { expect } = import('chai');
-var wordsJS = import("../lib-to-test/src/words.js");
+//import words from "../lib-to-test/src/words.js";
 
-/* describe("words.js -class doing class stuff", function() {
+//const words   = import("./../lib-to-test/src/words.js");
+import words from "./../lib-to-test/src/words.js";
+import { expect } from "chai";
+
+describe("words.js -class Splits `string` into an array of its words", function() {
     it("Splits `string` into an array of its words.", function() {
-        var query1 = wordsJS("Mustard green wall carpet")
+        var query1 = words("Mustard green wall carpet")
         var query2 = "Mustardgreenwallcarpet"
-        var query3 = "Mustard greenwall carpet"
-        var query4 = "Mustard&greenwall&carpet"
-        var query5 = "Mustard&()¤(%/&%green¤%//¤%¤/wall¤%&¤&%¤&carpet"
+        // var query3 = "Mustard greenwall carpet"
+        // var query4 = "Mustard&greenwall&carpet"
+        // var query5 = "Mustard&()¤(%/&%green¤%//¤%¤/wall¤%&¤&%¤&carpet"
+
+        const answer1 = ["Mustard", "green", "wall", "carpet"]
+        const answer2 = "Mustardgreenwallcarpet"
   
-        expect(query1).to.equal("Mustard green wall carpet");
-        expect(query2).to.equal("Mustardgreenwallcarpet");
-        expect(query3).to.equal("Mustard greenwall carpet");
-        expect(query4).to.equal("Mustard&greenwall&carpet");
-        expect(query5).to.equal("Mustard&()¤(%/&%green¤%//¤%¤/wall¤%&¤&%¤&carpet");
+        expect(query1).to.deep.equal(answer1)
+        expect(query2).to.deep.equal("Mustardgreenwallcarpet")
+        // expect(query3).to.equal("Mustard greenwall carpet");
+        // expect(query4).to.equal("Mustard&greenwall&carpet");
+        // expect(query5).to.equal("Mustard&()¤(%/&%green¤%//¤%¤/wall¤%&¤&%¤&carpet");
     });
-}); */
+});
